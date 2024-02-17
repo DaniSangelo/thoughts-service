57cbb27b348393e8b7d5c1bc0a136fb8a73a9cf7
Daniel Sângelo <danielsangelo@hotmail.com>
2024-02-17 18:06:33 -0300

    Log in user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,36 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs');
 module.exports = class AuthController {
-    static login(req, res) {
+    static loginForm(req, res) {
         res.render('auth/login')
+    }
+
+    static async login(req, res) {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({raw: true, where: { email: email }});
+
+        if (!user) {
+            req.flash('message', 'Credenciais inválidas');
+            res.render('auth/login')    
+            return
+        }
+
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+
+        if (!passwordMatch) {
+            req.flash('message', 'Credenciais inválidas');
+            res.render('auth/login')    
+            return
+        }
+
+        req.session.userid = user.id;
+        req.flash('message', `Seja bem-vindo ${user.name}`)
+
+        req.session.save(() => {
+            res.redirect('/')
+        })
+        
     }
 
     static logout(req, res) {
